4d0a3f5d9140f4c0447278a47c1672e31a13ee22
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-02 20:50:42 -0400

    Ex1 done

---
+++ reinforcement-exercise.rb
@@ -0,0 +1,15 @@
+# require 'activesupport'
+
+list = (1..25).to_a  #setup the list
+
+def add_odd(list)
+  sum = 0
+  list.each { |num|
+    if num % 2 == 0
+      sum += num
+    end
+   }
+   sum
+end
+
+puts add_odd(list)
